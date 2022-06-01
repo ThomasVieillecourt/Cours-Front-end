601b879b96f4a0573e9e6395037573ee0b7543eb
Thomas Vieillecourt <thomas.viei@hotmail.fr>
2022-06-01 16:28:23 +0200

    Cours API JS

--- Cours API/Cours/main.js
+++ Cours API/Cours/main.js
@@ -43,23 +43,23 @@ fetch("data.json").then((res) => console.log(res));
 
 // CRUD = Create(POST), Read (get), Update(put), Delete(delete)
 
-const init2 = {
-    method: "POST",
-    headers: {
-        "Content-Type": "application/json"
-    },
-    body: JSON.stringify({
-        pseudo: "Fantasio",
-        message: "Joue aux jeux-vidéos",
-    }),
-    mode: "cors",
-    credentials: "same-origin",
-};
+// const init2 = {
+//     method: "POST",
+//     headers: {
+//         "Content-Type": "application/json"
+//     },
+//     body: JSON.stringify({
+//         pseudo: "Fantasio",
+//         message: "Joue aux jeux-vidéos",
+//     }),
+//     mode: "cors",
+//     credentials: "same-origin",
+// };
 
 
-document.querySelector("form"); addEventListener("submit", () => {
-    fetch('http://localhost:3000/posts', init2).then(() => console.log('data envoyée'))
-});
+// document.querySelector("form"); addEventListener("submit", () => {
+//     fetch('http://localhost:3000/posts', init2).then(() => console.log('data envoyée'))
+// });
 
 
 
@@ -84,3 +84,55 @@ async function fetchData() {
 
     executeFunction();
 }
+
+
+// --- JSON = JavaScript Object Notation --- //
+
+/// Méthode .json() => méthode qui s'auto-résout en renvoyant le bidy de la requete
+
+fetch('data.json').then((res) => res.json()).then((data) => {
+    // Stringify converti en JSON (objet json)
+    let settings = JSON.stringify(data);
+    // Parse transforme en json en objet JS
+    console.log(JSON.parse(settings));
+});
+
+
+
+// -----------
+// Web API
+// -----------
+
+// --- Client Storage ---
+//Local Storage
+
+// Capacité de stockage cookie = 4ko
+// capacité LocalStorage = 10Mo
+localStorage.data = "Je stock la data"; // Stocker des éléments = localStorage.variable = contenu
+
+// document.body.textContent = localStorage.data;
+localStorage.removeItem('data');
+
+localStorage.user = "Denis";
+// Il faut passer des chaines de caractères 
+
+const obj = {
+    name: "Thomas",
+    age: 23,
+}
+localStorage.data = JSON.stringify(obj);
+// permet d'afficher des obj ou tableau en les convertissant en obj JSON
+
+
+// Session Storage //
+sessionStorage.dataSettings = "55px";
+sessionStorage.clear();
+
+// Cookies // => 
+document.cookie = "username=Thomas;path=/;max-age=45000;secure; samesite";
+
+// " information, chemin, duration, sécurité, site "
+
+
+
+
