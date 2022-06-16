9eba6e9a30be4007f4a80182f81fd9baecb2c5fe
Thomas Vieillecourt <thomas.viei@hotmail.fr>
2022-06-16 09:24:43 +0200

    Test, Canvas, Import

---
+++ Cours JS 2/Import_plusieurs_fichiers/add.js
@@ -0,0 +1,13 @@
+// export function addNum(a, b) {
+//     return a + b;
+// }
+
+// export permet de l'appeler dans tout le projet
+
+function addNum(a, b) {
+    return a + b;
+}
+
+// pour jest 
+
+module.exports = addNum;
---
+++ Cours JS 2/Import_plusieurs_fichiers/index.js
@@ -0,0 +1,5 @@
+// Pour importer d'autres fichiers il faut passer par NodeJS / npm parcel
+
+// import { addNum } from "./add.js";
+
+// console.log(addNum(4, 6));
